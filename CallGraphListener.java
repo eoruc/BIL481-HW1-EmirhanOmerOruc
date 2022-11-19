084b957945649655ea27131500ded2d6db05a45c
emirhan.oruc <emirhan.oruc@hotmail.com>
2022-11-19 14:12:52 +0300

    Initial

---
+++ CallGraphListener.java
@@ -0,0 +1,48 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.misc.*;
+import java.util.*;
+
+
+public class CallGraphListener extends Java8BaseListener {
+
+  @Override
+  public void enterPackageDeclaration(Java8Parser.PackageDeclarationContext ctx) {
+    // TODO Auto-generated method stub
+    super.enterPackageDeclaration(ctx);
+  }
+  
+  @Override
+  public void exitPackageDeclaration(Java8Parser.PackageDeclarationContext ctx) {
+    // TODO Auto-generated method stub
+    super.exitPackageDeclaration(ctx);
+  }
+
+  @Override
+  public void enterNormalClassDeclaration(Java8Parser.NormalClassDeclarationContext ctx) {
+    // TODO Auto-generated method stub
+    super.enterNormalClassDeclaration(ctx);
+  }
+
+  @Override
+  public void exitNormalClassDeclaration(Java8Parser.NormalClassDeclarationContext ctx) {
+    // TODO Auto-generated method stub
+    super.exitNormalClassDeclaration(ctx);
+  }
+
+  @Override
+  public void enterMethodDeclarator(Java8Parser.MethodDeclaratorContext ctx) {
+    // TODO Auto-generated method stub
+    super.enterMethodDeclarator(ctx);
+  }
+
+  @Override
+  public void exitMethodDeclarator(Java8Parser.MethodDeclaratorContext ctx) {
+    // TODO Auto-generated method stub
+    super.exitMethodDeclarator(ctx);
+  }
+
+  
+  
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # BIL481-HW1-EmirhanOmerOruc
 BIL481 Homework1 Repository by Emirhan Omer Oruc
+
+#Steps
+1. Antlr installed.
+2. Two commands runned successfully.
+3. Graphviz installed.
+4. Repository folder created.
+5. Done.
+6. Done.
+7. Done. -> antlr4 Java8.g4 -> I have an alias which is "alias antlr4="java -jar $ANTLR_JAR"
+8. Seen.
+9. Done.
+10. Done.
+11. Done. -> grun Java8 compilationUnit -gui -> I have an alias which is "alias grun="java org.antlr.v4.gui.TestRig""
+12. Done.
+13. Done.
+14. Done.
+15. Done.
+16. 
+
